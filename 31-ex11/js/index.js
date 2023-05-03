946529ed3b8892def240a3501df8479d3c9e6b8c
eilmadc@gmail.com <eilmadc@gmail.com>
2023-05-03 09:19:08 +0200

    Initial commit

---
+++ 31-ex11/js/index.js
@@ -0,0 +1,4 @@
+/* Imprimo por consola del navegador el mensaje:  Hola Mundo*/
+console.log("Hola Mundo!");
+/* Imprimo por consola del navegador el mensaje:  Soy el primer Script*/
+console.log("Soy el primer script");
---
+++ README.md
@@ -0,0 +1,2 @@
+# eilmadc-c5-ud31-javascriptii
+ C5 ud31 Javascript(ii)
