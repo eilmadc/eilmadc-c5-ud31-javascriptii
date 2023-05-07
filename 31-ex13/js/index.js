495220f6c3a0573c0ce109e9650eaa0cb8036b16
eilmadc@gmail.com <eilmadc@gmail.com>
2023-05-07 22:04:47 +0200

    add exercise13

---
+++ 31-ex13/js/index.js
@@ -0,0 +1,26 @@
+console.log("-------------- 31-Ejercicio 13-------------");
+
+//Todos los parrafos del documento
+document.getElementById("p1").onclick =() => {
+    alert("Se presionó un párrafo del documento");
+};
+document.getElementById("pt1-1").onclick =() => {
+    alert("Se presionó un párrafo del documento");
+};
+document.getElementById("pt1-2").onclick =() => {
+    alert("Se presionó un párrafo del documento");
+};
+document.getElementById("pt2-1").onclick =() => {
+    alert("Se presionó un párrafo del documento");
+};
+document.getElementById("pt2-2").onclick =() => {
+    alert("Se presionó un párrafo del documento");
+};
+
+//Los parrafos de la segunda tabla del documento
+document.getElementById("pt2-1").onclick =() => {
+    alert("Se presionó un párrafo contenido en la segunda tabla");
+};
+document.getElementById("pt2-2").onclick =() => {
+    alert("Se presionó un párrafo contenido en la segunda tabla");
+};
