a96e23cd16fdd2aa6bc8e0843d0fa3f6225e104e
eilmadc@gmail.com <eilmadc@gmail.com>
2023-05-11 00:12:32 +0200

    html buttons onclick

---
+++ 31-ex14/js/index.js
@@ -0,0 +1,23 @@
+
+/* Iniciar la pantalla de la calculadora al cargar la pagina.*/
+
+windows.onload = () => {
+    screen = document.getElementById("calc_screen");
+}
+
+
+
+
+
+/* Retr */
+clickRetr = () => {
+}
+
+/* CE */
+clickCe = () => {
+}
+
+/* C */
+clickC = () => {
+
+}
