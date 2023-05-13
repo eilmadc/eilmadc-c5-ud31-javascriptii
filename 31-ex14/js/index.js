c05afcf2378556247f28f93a8bd90fa05c3cea37
eilmadc@gmail.com <eilmadc@gmail.com>
2023-05-13 02:39:54 +0200

    documentation

--- 31-ex14/js/index.js
+++ 31-ex14/js/index.js
@@ -12,17 +12,17 @@ resetCalc = () => {
 
 
 display = (val) => {
-    document.getElementById('calc_screen').value += val;
+    document.getElementById('calc_screen').value += val;//Añade los valores a la pantalla
 }
 
 clearScreen = () => {
-    document.getElementById('calc_screen').value = "";
+    document.getElementById('calc_screen').value = "";//Asignaa el valor en pantalla 0
 }
 
 calc = () => {
-    let x = document.getElementById('calc_screen').value;
-    let y = eval(x);
-    document.getElementById('calc_screen').value = y;
+    let x = document.getElementById('calc_screen').value;//Obtiene el valor
+    let y = eval(x);//Evalua la expresion del display
+    document.getElementById('calc_screen').value = y;//Muestra el valor en pantalla
     return y;
 }
 
@@ -32,36 +32,37 @@ calc = () => {
 clickRetr = () => { }
 
 clickChange = () => {
-    var val = document.getElementById("calc_screen");
-    val.value = -(val.value);
-    document.getElementById("calc_screen").value = val.value;
+    var val = document.getElementById("calc_screen"); //Obtiene el valor
+   
+    val.value = Math.sign(val.value)*val.value;//Cambia el signo
+    document.getElementById("calc_screen").value = val.value;//Muestra el valor en pantalla
 }
 
 /*Raiz*/
 clickRaiz = () => {
-    var val = document.getElementById("calc_screen");
-    val.value = Math.sqrt(val.value);
-    console.log(val.value);
-    document.getElementById("calc_screen").value = val.value;
+    var val = document.getElementById("calc_screen");//Obtiene el valor
+    val.value = Math.sqrt(val.value);//Raiz cuadrada
+    document.getElementById("calc_screen").value = val.value;//Muestra el valor en pantalla
  }
 
 
 /* Percentage */
 clickPercentage = () => {
-    var val = document.getElementById("calc_screen");
-    val.value = 1 / val.value;
-    document.getElementById("calc_screen").value = val.value;
+    var val = document.getElementById("calc_screen");//Obtiene el valor
+    val.value = 1 / val.value;//Percentage
+    document.getElementById("calc_screen").value = val.value;//Muestra el valor en pantalla
 }
 
 
 /* Inverse */
 clickInverse = () => {
-    var val = document.getElementById("calc_screen");
-    val.value = 1 / val.value;
-    document.getElementById("calc_screen").value = val.value;
+    var val = document.getElementById("calc_screen");//Obtiene el valor
+    val.value = 1 / val.value;//Inverse
+    document.getElementById("calc_screen").value = val.value;//Muestra el valor en pantalla
 
 }
 
+//Inicia la calculadora al cargar el body
 inic = () => {
-    val = document.getElementById("val");
+    val = document.getElementById("val"); 
 }
