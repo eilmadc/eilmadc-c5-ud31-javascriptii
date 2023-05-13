5955e223be8df7d956edddbea34798e4da29cf21
eilmadc@gmail.com <eilmadc@gmail.com>
2023-05-13 02:30:16 +0200

    final calc
    Pending retr

--- 31-ex14/js/index.js
+++ 31-ex14/js/index.js
@@ -1,23 +1,67 @@
 
 /* Iniciar la pantalla de la calculadora al cargar la pagina.*/
+var val;
+var acc = 0;// Guardael valero de la pantalla
+var op = "";
 
-windows.onload = () => {
-    screen = document.getElementById("calc_screen");
+resetCalc = () => {
+    val;
+    acc = 0;// Guardael valero de la pantalla
+    op = "";//Selecciona la operacion pulsada
+};
+
+
+display = (val) => {
+    document.getElementById('calc_screen').value += val;
 }
 
+clearScreen = () => {
+    document.getElementById('calc_screen').value = "";
+}
 
+calc = () => {
+    let x = document.getElementById('calc_screen').value;
+    let y = eval(x);
+    document.getElementById('calc_screen').value = y;
+    return y;
+}
 
 
 
 /* Retr */
-clickRetr = () => {
+clickRetr = () => { }
+
+clickChange = () => {
+    var val = document.getElementById("calc_screen");
+    val.value = -(val.value);
+    document.getElementById("calc_screen").value = val.value;
 }
 
-/* CE */
-clickCe = () => {
+/*Raiz*/
+clickRaiz = () => {
+    var val = document.getElementById("calc_screen");
+    val.value = Math.sqrt(val.value);
+    console.log(val.value);
+    document.getElementById("calc_screen").value = val.value;
+ }
+
+
+/* Percentage */
+clickPercentage = () => {
+    var val = document.getElementById("calc_screen");
+    val.value = 1 / val.value;
+    document.getElementById("calc_screen").value = val.value;
 }
 
-/* C */
-clickC = () => {
 
+/* Inverse */
+clickInverse = () => {
+    var val = document.getElementById("calc_screen");
+    val.value = 1 / val.value;
+    document.getElementById("calc_screen").value = val.value;
+
+}
+
+inic = () => {
+    val = document.getElementById("val");
 }
